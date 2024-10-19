4d7b066a23388063976de32c0f4d04361aea8667
Navdeep <navdeep@Navdeeps-MacBook-Pro.local>
2024-10-19 08:03:31 -0700

    2nd Deploy for vercel

--- src/components/ProctectedRoute.tsx
+++ src/components/ProctectedRoute.tsx
@@ -6,10 +6,16 @@ const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const router = useRouter();
-  const token = localStorage.getItem('token');
-  if (!token) {
-    router.push('/auth/login');
+
+  const isBrowser = typeof window !== 'undefined';
+
+  if (isBrowser) {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      router.push('/auth/login');
+    }
   }
+
   return <>{children}</>;
 };
 
